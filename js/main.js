6c637a38a570f5c1ca34df9c0ff457c7929f840d
unknown <ghkdlxm3565@naver.com>
2023-10-30 20:34:39 +0900

    Feat(Swiper Slide_2)

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ const slide01 = new Swiper('.swiper-container_01',{
     },
     speed : 1000
 });
+
+const slide02 = new Swiper('.swiper-container_02',{
+    loop : true, 
+    scrollbar : {
+        el : '.section_2 .scrollBar'
+    },
+    slidesPerView : 3,
+    spaceBetween : 30,
+    autoplay : {
+        delay : 3000
+    },
+    speed : 1000
+});
